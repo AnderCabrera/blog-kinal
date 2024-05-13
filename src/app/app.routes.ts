4a5ca6f7ab7560362a9c69492fd1ee3bb7e7b9a1
Ander Cabrera <andercabrerac@gmail.com>
2024-05-13 09:54:15 -0600

    adding taller view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { RootComponent } from './pages/root/root.component';
+import { TallerComponent } from './pages/taller/taller.component';
 
 export const routes: Routes = [
   { path: '', component: RootComponent },
+  { path: 'taller', component: TallerComponent },
 ];
--- src/app/pages/taller/taller.component.ts
+++ src/app/pages/taller/taller.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-taller',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, NgFor],
   templateUrl: './taller.component.html',
   styleUrl: './taller.component.scss'
 })
 export class TallerComponent {
+  primerBimestre: Array<any> = [
+    {
+      title: 'Laboratorio #1',
+      description: 'Aplicación web para la gestión de tareas escolares',
+      link: 'https://github.com/AnderCabrera/multi-homework'
+    },
+    {
+      title: 'Laboratorio #2',
+      description: 'Sistema de gestión de mascotas',
+      link: 'https://github.com/acabrera-2022508/laboratorio2.git'
+    },
+    {
+      title: 'Proyecto Final',
+      description: 'Revisión de avances en proyecto final de bimestre',
+      link: 'https://github.com/acabrera-2022508/sales-backend/tree/feature-routes'
+    },
+    {
+      title: 'Proyecto Final Bim.',
+      description: 'Entrega de proyecto bimestral "Ventas Online"',
+      link: 'https://github.com/acabrera-2022508/sales-backend/tree/feature-routes'
+    },
+  ];
 
+  segundoBimestre: Array<any> = [
+    {
+      title: 'HackerRank',
+      description: 'Ejercicios sobre lógica de programación',
+      link: './assets/files/hackerrank.pdf'
+    }
+  ];
+
+  openLink(url: string) {
+    window.open(url, '_blank');
+  }
 }
